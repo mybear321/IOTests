33408d958283cafe7124798c0ec29473631f04e0
Administrator <838724609@qq.com>
2019-01-30 21:49:03 +0800

    create my first git project push to remote repository

---
+++ IOTests/src/com/bjsxt/day01/FileTests.java
@@ -0,0 +1,10 @@
+package com.bjsxt.day01;
+
+import java.io.File;
+
+public class FileTests {
+	public static void main(String[] args) {
+		File file = new File("D:/1a.txt");
+		System.out.println(file.exists()+" ...kan kan zai bu zai");
+	}
+}
